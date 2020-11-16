e80f081f90b60af055b797e453caec38a960c1b4
aragaomateus <aragaosm@icloud.com>
2020-11-16 14:23:25 -0500

    Update FoodMenu.java

--- RestaurantPOS/FoodMenu.java
+++ RestaurantPOS/FoodMenu.java
@@ -1,11 +1,4 @@
 public class FoodMenu {
-    /*private field for food menu.
-    Entree = Mussarela sticks, Ceasar Salad
-    arrays list:
-    Main Course[] = Chicken Parmigiana, Steak Frites.
-    Dessert [] = ice cream, tiramisu, cheesecake.
-    Drinks [] = Caipirinha, orange juice, negroni. 
-
-     */
+ 
 
 }
